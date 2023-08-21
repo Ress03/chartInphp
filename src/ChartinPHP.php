b5eab1cc61e62d6208050746736e31302753239e
SANNN <115645790+Ress03@users.noreply.github.com>
2023-08-21 09:13:11 +0800

    Add files via upload

---
+++ src/ChartinPHP.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace reysandev;
+
+class ChartinPHP {
+
+    private $chartOptions;
+    private $chartData;
+    private $chartID;
+
+    public function __construct($chartOptions, $chartData, $chartID = null) {
+        $this->chartOptions = json_encode($chartOptions, JSON_NUMERIC_CHECK);
+        $this->chartData = json_encode($chartData, JSON_NUMERIC_CHECK);
+        $this->chartID = $chartID ?: uniqid('chart_');
+    }
+
+    public function renderChart() {
+        echo '<div id="' . $this->chartID . '"></div>';
+        echo '<script>';
+        echo 'var ' . $this->chartID . ' = new ApexCharts(document.getElementById("' . $this->chartID . '"), ' . $this->chartOptions . ');';
+        echo $this->chartID . '.render();';
+        echo '</script>';
+    }
+
+    public function getChartOptions() {
+        return $this->chartOptions;
+    }
+
+    public function setChartOptions($chartOptions) {
+        $this->chartOptions = json_encode($chartOptions, JSON_NUMERIC_CHECK);
+    }
+
+    public function getChartData() {
+        return $this->chartData;
+    }
+
+    public function setChartData($chartData) {
+        $this->chartData = json_encode($chartData, JSON_NUMERIC_CHECK);
+    }
+
+    public function getChartID() {
+        return $this->chartID;
+    }
+
+    public function setChartID($chartID) {
+        $this->chartID = $chartID;
+    }
+}
+
+?>
